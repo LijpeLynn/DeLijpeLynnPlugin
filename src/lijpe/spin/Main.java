b63b88cd0024a8be9a49f9ca06f7f5e01c1d1071
Danie <slimmedaan@gmail.com>
2023-12-06 21:00:59 +0100

    removed oncommand from main

--- src/lijpe/spin/Main.java
+++ src/lijpe/spin/Main.java
@@ -2,13 +2,8 @@ package lijpe.spin;
 
 import lijpe.spin.commands.*;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Map;
 import java.util.logging.Logger;
 
 public class Main extends JavaPlugin {
@@ -32,18 +27,5 @@ public class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         logger.info(pluginName+" is rustig afgesloten! ("+version+")");
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        if (label.equalsIgnoreCase("welkom")) {
-
-        }
-        else if (label.equalsIgnoreCase("spreekals")){
-
-        }
-
-        return false;
     }
 }
